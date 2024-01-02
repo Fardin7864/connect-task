461a30506c0a6dd2fc9434666fc9ef7e540b31e6
Fardin7864 <fardinhasan1888@gmail.com>
2024-01-03 01:33:03 +0600

    add hader style and drawer

--- App.js
+++ App.js
@@ -1,31 +1,44 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useState } from 'react';
+import { View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Signup from './screens/signup/Signup';
 import Signin from './screens/signin/Signin';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  const [open, setOpen] = useState(false);
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name='Signup' component={Signup}/>
-        <Drawer.Screen name='Signin' component={Signin}/>
-      </Drawer.Navigator>
+        <Drawer.Navigator
+          screenOptions={{
+            drawerItemStyle: {
+              borderBottomWidth: 1,
+              borderBottomColor: 'black',
+              shadowColor: 'black',
+            },
+            drawerActiveBackgroundColor: '#3697e5',
+            drawerActiveTintColor: "#FF9800",
+            drawerInactiveTintColor: 'white',
+            drawerContentStyle: {
+              backgroundColor: '#3679e0'
+            }
+          }}
+        >
+          <Drawer.Screen 
+          name="Signup" 
+          component={Signup}
+          options={{
+            headerBackground: () =>  <LinearGradient
+            colors={["#3697e5", "#3679e0"]}
+            start={{ y: 1, x: 0.9 }}
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          />,
+          }}
+          />
+          <Drawer.Screen name="Signin" component={Signin} />
+        </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
